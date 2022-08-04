e64f96b7f9464309f1bbb5fdbb32443a31d5b3b7
Ng Han Yi <hanyi.hyng@gmail.com>
2022-08-05 00:06:32 +0800

    :bug: add log

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ export const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
     if (req.nextUrl.pathname?.startsWith("/api/get-url/")) return;
 
     const slug = req.nextUrl.pathname.split("/").pop();
+    console.log(`SLUG: ${slug}`);
     if (!slug) return;
 
     const data = await (
